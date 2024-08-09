05a49967608108af616e1d217429a15d49aa4074
ajin <aj5210@naver.com>
2024-08-09 16:15:37 +0900

    :sparkles: Feat(create-board): 게시판 생성하기 hero 추가
    relate to: #161

--- src/app/(main)/activity/create-board/page.tsx
+++ src/app/(main)/activity/create-board/page.tsx
@@ -1,5 +1,11 @@
+import { CreateBoardHero } from './_components/CreateBoardHero'
+
 const CreateBoardPage = () => {
-  return <div>create board</div>
+  return (
+    <div className="w-full px-8 pt-10 md:px-20">
+      <CreateBoardHero />
+    </div>
+  )
 }
 
 export default CreateBoardPage
