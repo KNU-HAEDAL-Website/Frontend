6d4faf4ac1f168352ad493d29a711006de7f75f6
ajin <aj5210@naver.com>
2024-08-13 23:32:14 +0900

    :sparkles: Feat(create-post): 게시글 생성 페이지 hero 추
    related to: #169

--- src/app/(main)/activity/[semesterName]/[activityId]/boards/[boardId]/create-post/page.tsx
+++ src/app/(main)/activity/[semesterName]/[activityId]/boards/[boardId]/create-post/page.tsx
@@ -1,5 +1,21 @@
-const CreatePostPage = () => {
-  return <div>create post</div>
+import { CreatePostHero } from './_components/CreatePostHero'
+
+type CreatePostPageParams = {
+  params: {
+    activityId: string
+    boardId: string
+  }
+}
+
+const CreatePostPage = ({ params }: CreatePostPageParams) => {
+  return (
+    <div className="pt-10">
+      <CreatePostHero
+        activityId={Number(params.activityId)}
+        boardId={Number(params.boardId)}
+      />
+    </div>
+  )
 }
 
 export default CreatePostPage
