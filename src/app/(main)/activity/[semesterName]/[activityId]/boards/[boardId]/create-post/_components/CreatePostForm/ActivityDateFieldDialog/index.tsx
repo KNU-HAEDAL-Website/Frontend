19e3f49f4c60de7744c98d3d5dc7da92791e4171
ajin <aj5210@naver.com>
2024-08-14 20:43:57 +0900

    :sparkles: Feat(create-post): 활동 날짜 input calendar UI 추가
    - shadcn ui와 react-day-picker 버전 에러로 date 세팅 안됨
    -  해당 에러 해결된 이후 코드 수정하기
    
    related to: #169

--- src/app/(main)/activity/[semesterName]/[activityId]/boards/[boardId]/create-post/_components/CreatePostForm/ActivityDateFieldDialog/index.tsx
+++ src/app/(main)/activity/[semesterName]/[activityId]/boards/[boardId]/create-post/_components/CreatePostForm/ActivityDateFieldDialog/index.tsx
@@ -1,32 +1,28 @@
 import { useFormContext } from 'react-hook-form'
 
-import { Label } from '@radix-ui/react-dropdown-menu'
-import { kstFormat } from '@toss/date'
-
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
+  DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog'
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form'
+import { FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Label } from '@/components/ui/label'
 import { CreatePost } from '@/schema/post'
 
 import { DateDialogTriggerButton } from './DateDialogTriggerButton'
 
 export const ActivityDateFieldDialog = () => {
-  const form = useFormContext<CreatePost>()
+  const { control, setValue } = useFormContext<CreatePost>()
 
   return (
     <FormField
-      control={form.control}
+      control={control}
       name="activityDate"
       render={({ field }) => (
         <FormItem className="flex flex-col">
@@ -39,8 +35,37 @@ export const ActivityDateFieldDialog = () => {
                   endDate={field.value.end}
                 />
                 <DialogContent>
-                  <DialogTitle>test</DialogTitle>
-                  <DialogDescription>gg</DialogDescription>
+                  <DialogHeader>
+                    <DialogTitle>활동 날짜 선택하기</DialogTitle>
+                    <DialogDescription>
+                      시작일과 종료일을 선택해주세요.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex justify-center">
+                    <Calendar
+                      mode="range"
+                      selected={{
+                        from: field.value.start!,
+                        to: field.value.end,
+                      }}
+                      onSelect={(range) => field.onChange(range)}
+                    />
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        className="w-fit"
+                        onClick={() => {
+                          setValue('activityDate', {
+                            start: new Date(), //TODO: field.value.start
+                            end: field.value.end,
+                          })
+                        }}
+                      >
+                        저장하기
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
                 </DialogContent>
               </Dialog>
             </div>
