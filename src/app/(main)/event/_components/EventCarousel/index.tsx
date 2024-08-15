1710c5706387163f794c3b6c519a581786b461d9
ajin <aj5210@naver.com>
2024-08-15 21:54:49 +0900

    :lipstick: Design(event): 행사 게시글 슬라이더 UI 추가
    related to: #175

---
+++ src/app/(main)/event/_components/EventCarousel/index.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+
+export function EventCarousel() {
+  return (
+    <Carousel
+      opts={{ loop: true }}
+      className="xs:max-w-sm w-full max-w-52 sm:max-w-xl"
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-video flex-col p-0">
+                  <div className="flex w-full flex-1 items-center justify-center">
+                    게시글 이미지
+                  </div>
+                  <div className="flex flex-col gap-2 bg-slate-100 p-4">
+                    <div className="font-semibold">게시글 제목</div>
+                    <div className="text-sm">게시글 내용</div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
--- src/app/(main)/event/page.tsx
+++ src/app/(main)/event/page.tsx
@@ -1,5 +1,11 @@
+import { EventCarousel } from './_components/EventCarousel'
+
 const EventPage = () => {
-  return <div>event</div>
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-2 px-20 sm:max-w-screen-xl">
+      <EventCarousel />
+    </div>
+  )
 }
 
 export default EventPage
