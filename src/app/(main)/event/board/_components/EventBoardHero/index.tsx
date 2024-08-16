040e958da3a3599d199ba9ea4ffc7a29a392fda0
ajin <aj5210@naver.com>
2024-08-16 21:35:03 +0900

    :lipstick: Design(event-board): 행사 게시판 페이지 breadcrumb 추가
    related to: #175

--- src/app/(main)/event/board/_components/EventBoardHero/index.tsx
+++ src/app/(main)/event/board/_components/EventBoardHero/index.tsx
@@ -1,17 +1,23 @@
+import { NavLink, PageBreadcrumb } from '@/components/PageBreadcrumb'
 import { Seperator } from '@/components/ui/seperator'
+
+import { EventBoardDetail } from './EventBoardDetail'
 
 export const EventBoardHero = () => {
   return (
-    <div className="flex w-full flex-col gap-4">
+    <div className="flex w-full flex-col">
       <Seperator variant="dark" />
-      <div className="pl-1 text-2xl font-semibold">행사 게시판</div>
-      <div className="flex flex-col gap-1 pl-1 text-sm">
-        <div>🗓️ 개강총회/종강총회/성과공유회</div>
-        <div>🎉 해크닉/MT/슬기로운 해달생활</div>
-        <div>💡 아이디어톤/해커톤</div>
-        <div>🎓 진로특강/취업특강</div>
-      </div>
+      <PageBreadcrumb navLinks={EventPageNavLinks} pageName="행사 게시판" />
+      <EventBoardDetail />
       <Seperator variant="dark" />
     </div>
   )
 }
+
+const EventPageNavLinks: NavLink[] = [
+  {
+    index: 0,
+    link: '/event',
+    name: '행사 갤러리',
+  },
+]
