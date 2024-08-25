7a3a58f5b2ec8152d3b63641f785a3531c272ce8
ajin <aj5210@naver.com>
2024-08-25 15:48:25 +0900

    :sparkles: Feat(notice): 공지사항 게시판 추가
    related to: #184

--- src/app/(main)/notice/page.tsx
+++ src/app/(main)/notice/page.tsx
@@ -1,5 +1,17 @@
-const NoticePage = () => {
-  return <div>notice</div>
+import { CreateNoticePostButton } from './_components/CreateNoticePostButton'
+import { NoticeBoardHero } from './_components/NoticeBoardHero'
+import { NoticePostListSection } from './_components/NoticePostListSection'
+
+const NoticeBoardPage = () => {
+  return (
+    <div className="flex w-full max-w-screen-xl flex-col gap-10 px-12 pb-20 pt-10 sm:px-20">
+      <NoticeBoardHero />
+      <NoticePostListSection />
+      <div className="flex w-full justify-end">
+        <CreateNoticePostButton />
+      </div>
+    </div>
+  )
 }
 
-export default NoticePage
+export default NoticeBoardPage
