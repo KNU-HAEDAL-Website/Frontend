80ce9afb93956fa5e8c947c4e1ff7b944fbfb79e
ajin <aj5210@naver.com>
2024-08-25 12:58:24 +0900

    :sparkles: Feat(event-post): 행사 게시글 페이지 추가
    related to: #181

--- src/app/(main)/event/board/create-post/page.tsx
+++ src/app/(main)/event/board/create-post/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { CreateEventPostForm } from './_components/CreateEventPostForm'
+import { CreateEventPostHero } from './_components/CreateEventPostHero'
 
 const CreateEventPost = () => {
   return (
     <div className="flex flex-col gap-6">
-      {/* {/* <CreateEventPostHero /> */}
+      <CreateEventPostHero />
       <CreateEventPostForm />
-      ??
     </div>
   )
 }
---
+++ src/app/(main)/event/board/posts/[postId]/_components/EventPostSection/index.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
+import { PostDetail } from '@/components/PostView/PostDetail'
+import { useGetPost } from '@/service/data/post'
+
+type EventPostSectionProps = {
+  postId: number
+}
+
+const PostContent = dynamic(() => import('@/components/PostView/PostContent'), {
+  ssr: false,
+  loading: () => <div>loading...</div>,
+})
+
+export const EventPostSection = ({ postId }: EventPostSectionProps) => {
+  const { data: post } = useGetPost({ postId })
+
+  if (!post) return <div>게시글 정보가 없습니다.</div>
+
+  return (
+    <div>
+      <PostDetail post={post} />
+      <PostContent content={post.postContent} />
+    </div>
+  )
+}
