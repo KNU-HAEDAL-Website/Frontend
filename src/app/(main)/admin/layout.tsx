908108622d4a728e3a250f9eab243c5be6026e13
ajin <aj5210@naver.com>
2024-08-29 20:48:27 +0900

    :sparkles: Feat(admin): 관리자 페이지 접근 시 권한 확인 추가
    related to: #198

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,6 +1,23 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useMyInfoStore } from '@/store/myInfo'
+
 import { AdminSidebar } from './_components/AdminSidebar'
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const { role } = useMyInfoStore((state) => state.getMyInfo())
+  const router = useRouter()
+
+  useEffect(() => {
+    if (role !== '해구르르') {
+      router.replace('/')
+    }
+  }, [role])
+
   return (
     <div className="flex h-full w-full flex-1">
       <div className="flex-1 bg-slate-200">
